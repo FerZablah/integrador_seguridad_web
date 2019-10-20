6c5dc4d1e3515c380314f16a314d204d91b341f9
Zablon18 <zablah.developer@gmail.com>
2019-10-20 10:48:48 -0500

    added console log for env

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,2 @@
 console.log('env:', process.env.tipo);
-export const BASE_URL = (process.env.tipo === "production") ? "https://seguridad-integrador.herokuapp.com/" : "http://localhost:4000";
+export const BASE_URL = (process.env.tipo !== "development") ? "https://seguridad-integrador.herokuapp.com/" : "http://localhost:4000";
